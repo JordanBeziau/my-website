7003bf3715839a2388fdb623fe0b19d0599a018b
JordanBeziau <jordanbeziau@gmail.com>
2017-10-17 11:57:22 +0200

    Export setCanvas function in canvas component from file

---
+++ src/utils/canvasFunction.js
@@ -0,0 +1,96 @@
+export default (deviceWidth, properties) => {
+  // Code to actually run canvas stuff
+  const header = document.querySelector('.App-header')
+  const canvas = document.querySelector('canvas')
+  const ctx = canvas.getContext('2d')
+  //ctx    = canvas.getContext('2d').scale(2,2),
+  // idle   = null, mousePosition
+
+  canvas.width = deviceWidth * 2
+  canvas.height = header.offsetHeight * 2
+  canvas.style.width = deviceWidth+'px'
+  canvas.style.height = header.offsetHeight+'px'
+  canvas.style.display = 'block'
+
+  ctx.fillStyle = properties.color
+  //ctx.lineWidth = .5
+  ctx.lineWidth = 1.5
+  ctx.strokeStyle = properties.color
+
+  // var dotNum = properties.numParticles
+  var mousePosition = { x: 30 * canvas.width / 100, y: 30 * canvas.height / 100 },
+    //dots = { nb: 600, distance: 80, d_radius: 3000, array: [] }
+    dots = { nb: properties.numParticles, distance: properties.lineDistance, d_radius: 3000, array: [] }
+
+  function Dot() {
+    this.x = Math.random() * canvas.width
+    this.y = Math.random() * canvas.height
+
+    this.vx = -.5 + Math.random()
+    this.vy = -.5 + Math.random()
+
+    this.radius = Math.random()
+  }
+  let dot
+  Dot.prototype = {
+
+    create: function() {
+      ctx.beginPath()
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+      ctx.fill()
+    },
+
+    animate: function() {
+
+      for (let i = 0, dot=false; i < dots.nb; i++) {
+
+        dot = dots.array[i]
+
+        if (dot.y < 0 || dot.y > canvas.height) {
+          dot.vx = dot.vx
+          dot.vy = - dot.vy
+        } else if (dot.x < 0 || dot.x > canvas.width) {
+          dot.vx = - dot.vx
+          dot.vy = dot.vy
+        }
+        dot.x += dot.vx
+        dot.y += dot.vy
+      }
+    },
+
+    line: function() {
+      for (let i = 0; i < dots.nb; i++) {
+        for (let j = 0; j < dots.nb; j++) {
+          const i_dot = dots.array[i]
+          const j_dot = dots.array[j]
+
+          if ((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance) {
+            if ((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius) {
+              ctx.beginPath()
+              ctx.moveTo(i_dot.x, i_dot.y)
+              ctx.lineTo(j_dot.x, j_dot.y)
+              ctx.stroke()
+              ctx.closePath()
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function createDots() {
+    requestAnimationFrame(createDots)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < dots.nb; i++) {
+      dots.array.push(new Dot())
+      dot = dots.array[i]
+
+      dot.create()
+    }
+
+    dot.line()
+    dot.animate()
+  }
+  createDots()
+
+}
